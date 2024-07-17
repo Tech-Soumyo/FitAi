13297177f4cfa08d13cfb698b5fe600d2ec539fb
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-17 16:44:30 +0530

    import Navbart component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,14 @@
+import Navbar from "@/components/Custom/Navbar";
+
 export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
