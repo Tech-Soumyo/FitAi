36e84b6bd3811502dfac46740b28a701b9a4007d
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-17 13:27:45 +0530

    profile page

---
+++ src/app/(app)/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProfilePage: React.FC = () => {
+  // ... your component logic here
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {/* ... your component content */}
+    </div>
+  );
+};
+
+export default ProfilePage;
